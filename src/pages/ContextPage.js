5b5046f1654b0de24c56febd2d2e2a75b3c45164
zhangpengsheng <zhangpengsheng@bolo.me>
2020-02-25 15:33:38 +0800

    update

---
+++ src/pages/ContextPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContextPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ContextPage</h3>
+      </div>
+    )
+  }
+}
---
+++ src/pages/ContextTypePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContextTypePage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ContextTypePage</h3>
+      </div>
+    )
+  }
+}
